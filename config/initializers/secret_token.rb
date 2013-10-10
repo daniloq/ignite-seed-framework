13bc982e87e7e89b4ffc2a34624fd9137c0c99e6
Danilo Quilaton <d.quilaton@yahoo.com>
2013-10-10 15:40:12 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeedFramework::Application.config.secret_key_base = '349623d4e612f1b8a071b280657889b260f18482ed8022d5bae68220e7cbdf177a26b7ee55bcdc9e6d7ee17a8e8e33cfa8da57ac2b52f2475bc959412bc34b1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SeedFramework::Application.config.session_store :cookie_store, key: '_seed_framework_session'
